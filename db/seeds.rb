18c7644c6ce68abf95a0103fc0a78e753e1de43c
Andrew G <tke578@gmail.com>
2020-10-23 10:21:00 -0700

    Adds some db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+normal_user = User.create!(email: 'normal_user@gmail.com', password: 'password', password_confirmation: 'password')
+editor_user = User.create!(email: 'editor_user@gmail.com', password: 'password', password_confirmation: 'password', roles: :editor)
+admin_user = User.create!(email: 'editor_user@gmail.com', password: 'password', password_confirmation: 'password', roles: :admin)
+Article.create!(title: 'Hello World', category: 'Art', content: 'Lorem Ipsum', user: editor_user)
+Article.create!(title: 'Hello World', category: 'Business', content: 'Lorem Ipsum', user: editor_user)
+Article.create!(title: 'Hello World', category: 'Finance', content: 'Lorem Ipsum', user: editor_user)
